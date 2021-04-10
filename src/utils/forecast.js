b5cea14ba8ccd5c6021b69ffe6da6a87762c2621
Jorge Cervera <jorge.cervera@adidas-group.com>
2021-04-10 18:36:32 +0200

    Wind data added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,14 +3,16 @@ const request = require('postman-request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=cd037191df9a62d2c118bac9450489ce&query='+ longitude + ',' + latitude
     request({url, json: true}, (error,response) => { 
-        const {weather_descriptions:description,temperature:temperature, feelslike: feelslike}=response.body.current
+        const {weather_descriptions:description,temperature:temperature, feelslike: feelslike,wind_speed: wind_speed, wind_dir: wind_dir}=response.body.current
         const {error:responseError} = response.body
         if (error) {
             callback('Unable to connect to weather service', undefined )
         } else if (responseError){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, description[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelslike + ' out')
+            callback(undefined, description[0] + '. It is currently ' + temperature + 
+            ' degrees out. It feels like ' + feelslike + ' out. The wind speed is ' + wind_speed + 
+            ' and the wind direction is ' + wind_dir)
         }
     })
 
